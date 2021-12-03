21bbcdea7e9b8382795811eaadcb210c82af93b2
Loick Baude <loick.baude@ig2i.centralelille.fr>
2021-12-03 18:45:34 +0100

    day3 part2 code improvments

--- day3/part2.py
+++ day3/part2.py
@@ -1,3 +1,18 @@
+def countZerosAndOnes(binaryNumbers, i):
+    count_zero = 0 # compteur de 0
+    count_one = 0 # compteur de 1
+
+    for j in range(0, len(binaryNumbers)):
+        if int(binaryNumbers[j][i]) == 0:
+            zerosNumbers.append(binaryNumbers[j])
+            count_zero += 1
+        elif int(binaryNumbers[j][i]) == 1:
+            onesNumbers.append(binaryNumbers[j])
+            count_one +=1
+    
+    return count_zero, count_one
+
+
 binaryNumbers = []
 
 # recuperation des data
@@ -15,19 +30,10 @@ binaryNumbers_copy = binaryNumbers
 # oxygen
 # on boucle sur l'ensemble des colonnes, de haut en bas
 while len(binaryNumbers) > 1 :
-    count_zero = 0 # compteur de 0
-    count_one = 0 # compteur de 1
     zerosNumbers = [] # contient les binaires avec le bit caractéristiques à 1
     onesNumbers = [] # contient les binaires avec le bit caractéristiques à 0
-
-
-    for j in range(0, len(binaryNumbers)):
-        if int(binaryNumbers[j][i]) == 0:
-            zerosNumbers.append(binaryNumbers[j])
-            count_zero += 1
-        elif int(binaryNumbers[j][i]) == 1:
-            onesNumbers.append(binaryNumbers[j])
-            count_one +=1
+    
+    count_zero, count_one = countZerosAndOnes(binaryNumbers, i)
     
     if(count_zero > count_one):
         binaryNumbers = zerosNumbers
@@ -38,24 +44,16 @@ while len(binaryNumbers) > 1 :
 
 oxygen = int(binaryNumbers[0], 2)
 
+
 i = 0
 binaryNumbers = binaryNumbers_copy
 # carbon
 # on boucle sur l'ensemble des colonnes, de haut en bas
 while len(binaryNumbers) > 1 :
-    count_zero = 0 # compteur de 0
-    count_one = 0 # compteur de 1
     zerosNumbers = [] # contient les binaires avec le bit caractéristiques à 1
     onesNumbers = [] # contient les binaires avec le bit caractéristiques à 0
 
-
-    for j in range(0, len(binaryNumbers)):
-        if int(binaryNumbers[j][i]) == 0:
-            zerosNumbers.append(binaryNumbers[j])
-            count_zero += 1
-        elif int(binaryNumbers[j][i]) == 1:
-            onesNumbers.append(binaryNumbers[j])
-            count_one +=1
+    count_zero, count_one = countZerosAndOnes(binaryNumbers, i)
     
     if(count_one < count_zero):
         binaryNumbers = onesNumbers
